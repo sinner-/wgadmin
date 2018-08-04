094c4030a7ac87386beb1c42f5e96350d5b5815a
Sina Sadeghi <sina.sa@gmail.com>
2018-08-04 19:14:34 +1000

    add leaselist

---
+++ wgadmin/api/leaselist.py
@@ -0,0 +1,42 @@
+from base64 import b64decode
+from flask_restful import Resource
+from flask_restful import reqparse
+from flask_restful import abort
+from wgadmin.db.mysql import query_db
+from wgadmin.db.mysql import get_db
+from wgadmin.util.token import check_auth
+
+class LeaseList(Resource):
+
+    @staticmethod
+    def get():
+        parser = reqparse.RequestParser()
+
+        parser.add_argument(
+            'Authorization',
+            type=str,
+            location='headers',
+            required=True,
+            help="Either blank or incorrect type."
+        )
+
+        args = parser.parse_args()
+
+        #check auth
+        auth = check_auth(args.Authorization)
+        if not auth or auth['role'] not in ("admin", "server"):
+            abort(403, message="Unauthorized.")
+
+        result = query_db(
+            '''
+            SELECT ip, pubkey
+            FROM leases
+            '''
+        )
+
+        leases = []
+
+        for row in result:
+            leases.append({"ip": row[0], "pubkey": row[1]})
+
+        return {"leases": leases}, 200
--- wgadmin/api/routes.py
+++ wgadmin/api/routes.py
@@ -3,8 +3,10 @@ from wgadmin.api.user import User
 from wgadmin.api.session import Session
 from wgadmin.api.pubkey import PublicKey
 from wgadmin.api.lease import Lease
+from wgadmin.api.leaselist import LeaseList
 
 api.add_resource(User, '/api/v1.0/user/<username>')
 api.add_resource(Session, '/api/v1.0/session')
 api.add_resource(PublicKey, '/api/v1.0/user/me/pubkeys')
 api.add_resource(Lease, '/api/v1.0/user/me/lease')
+api.add_resource(LeaseList, '/api/v1.0/user/all/lease')
